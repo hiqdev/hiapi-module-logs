e5de20070b3c80d3f1e7e3f888de7c72d2b01fb1
Andrii Vasyliev <sol@hiqdev.com>
2017-10-06 16:41:46 +0000

    csfixed

--- src/config/common.php
+++ src/config/common.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * User activity logs module for hiAPI
+ *
+ * @link      https://github.com/hiqdev/hiapi-module-logs
+ * @package   hiapi-module-logs
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2017, HiQDev (http://hiqdev.com/)
+ */
 
 return [
     'components' => [
