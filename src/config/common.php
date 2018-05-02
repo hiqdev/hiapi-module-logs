f874ffdd6aebf33e2ca7bf5256e6aff8b124b16f
Andrii Vasyliev <sol@hiqdev.com>
2018-05-02 16:27:35 +0000

    quickfixed

--- src/config/common.php
+++ src/config/common.php
@@ -9,15 +9,4 @@
  */
 
 return [
-    'components' => [
-        'autobus' => [
-            'branches' => [
-                'logs' => [
-                    'search' => [
-                        'class'  => \hiapi\modules\logs\commands\LogsSearchCommand::class,
-                    ],
-                ],
-            ],
-        ],
-    ],
 ];
